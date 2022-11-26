ccf34263baf965995d8dca8103b3ff482c9385e9
EmanuelScherer <emanuel.scherer@meta.com.br>
2022-11-26 11:32:32 -0300

    add template modal

--- src/main/kotlin/klaza/klaza_server/views/src/components/ModalEdit.vue
+++ src/main/kotlin/klaza/klaza_server/views/src/components/ModalEdit.vue
@@ -1,233 +1,221 @@
 <template>
-    <q-dialog v-model="show">
-        <q-card>
-            <q-card-section
-                class="row items-center items-center content-center bg-a"
-            >
-                <div class="text-h6 text-w text-center col-grow">
-                    Editar configurações do curso {{ localCourse.fullName }}
-                </div>
-                <q-btn
-                    icon="fa-solid fa-times"
-                    flat
-                    round
-                    dense
-                    color="w"
-                    v-close-popup
-                />
-            </q-card-section>
-            <q-card-section class="bg-b">
-                <div class="text-h6 text-w text-center">
+
+    <modal-template v-model="show" :title="`Editar configurações do curso ${localCourse.fullName}`">
+        <div class="text-h6 text-w text-center">
                     {{ localCourse.fullName }}
-                </div>
-                <div class="full-width column content-center">
-                    <div class="text-w row justify-between items-center" style="width: 370px;">
-                        <span>Ativar notificações para esse curso</span>
+        </div>
+        <div class="full-width column content-center">
+            <div class="text-w row justify-between items-center" style="width: 370px;">
+                <span>Ativar notificações para esse curso</span>
+                <q-toggle
+                    v-model="localCourse.actived"
+                    color="bk"
+                    checked-icon="fa-solid fa-check"
+                    unchecked-icon="fa-solid fa-times"
+                />
+            </div>
+            <div class="text-w row justify-between items-center" style="width: 370px;">
+                <span>Usar configuração global para esse curso</span>
+                <q-toggle
+                    v-model="localCourse.user_config['use_global']"
+                    color="bk"
+                    checked-icon="fa-solid fa-check"
+                    unchecked-icon="fa-solid fa-times"
+                />
+            </div>
+        </div>
+        <q-scroll-area>
+            <div class="row justify-center gap-15">
+                <fieldset class="config-group">
+                    <legend>Conteudo</legend>
+
+                    <div
+                        class="full-width row justify-between items-center text-w"
+                    >
+                        <span class="col-grow"
+                            >Notificar criação de conteudo</span
+                        >
                         <q-toggle
-                            v-model="localCourse.actived"
+                            v-model="
+                                localCourse.user_config[
+                                    'notify_create_content'
+                                ]
+                            "
                             color="bk"
                             checked-icon="fa-solid fa-check"
                             unchecked-icon="fa-solid fa-times"
                         />
                     </div>
-                    <div class="text-w row justify-between items-center" style="width: 370px;">
-                        <span>Usar configuração global para esse curso</span>
+
+                    <div
+                        class="full-width row justify-between items-center text-w"
+                    >
+                        <span>Notificar edição de conteudo</span>
                         <q-toggle
-                            v-model="localCourse.user_config['use_global']"
+                            v-model="
+                                localCourse.user_config[
+                                    'notify_edit_content'
+                                ]
+                            "
                             color="bk"
                             checked-icon="fa-solid fa-check"
                             unchecked-icon="fa-solid fa-times"
                         />
                     </div>
-                </div>
-                <q-scroll-area>
-                    <div class="row justify-center gap-15">
-                        <fieldset class="config-group">
-                            <legend>Conteudo</legend>
 
-                            <div
-                                class="full-width row justify-between items-center text-w"
-                            >
-                                <span class="col-grow"
-                                    >Notificar criação de conteudo</span
-                                >
-                                <q-toggle
-                                    v-model="
-                                        localCourse.user_config[
-                                            'notify_create_content'
-                                        ]
-                                    "
-                                    color="bk"
-                                    checked-icon="fa-solid fa-check"
-                                    unchecked-icon="fa-solid fa-times"
-                                />
-                            </div>
-
-                            <div
-                                class="full-width row justify-between items-center text-w"
-                            >
-                                <span>Notificar edição de conteudo</span>
-                                <q-toggle
-                                    v-model="
-                                        localCourse.user_config[
-                                            'notify_edit_content'
-                                        ]
-                                    "
-                                    color="bk"
-                                    checked-icon="fa-solid fa-check"
-                                    unchecked-icon="fa-solid fa-times"
-                                />
-                            </div>
-
-                            <div
-                                class="full-width row justify-between items-center text-w"
-                            >
-                                <span>Notificar exclusão de conteudo</span>
-                                <q-toggle
-                                    v-model="
-                                        localCourse.user_config[
-                                            'notify_delete_content'
-                                        ]
-                                    "
-                                    color="bk"
-                                    checked-icon="fa-solid fa-check"
-                                    unchecked-icon="fa-solid fa-times"
-                                />
-                            </div>
-                        </fieldset>
-                        <fieldset class="config-group">
-                            <legend>Prazo</legend>
-
-                            <div
-                                class="full-width row justify-between items-center text-w"
-                            >
-                                <span>Notificar prazo de entraga (2 dias)</span>
-                                <q-toggle
-                                    v-model="
-                                        localCourse.user_config[
-                                            'notify_deadline_2_days'
-                                        ]
-                                    "
-                                    color="bk"
-                                    checked-icon="fa-solid fa-check"
-                                    unchecked-icon="fa-solid fa-times"
-                                />
-                            </div>
-
-                            <div
-                                class="full-width row justify-between items-center text-w"
-                            >
-                                <span>Notificar prazo de entraga (1 dia)</span>
-                                <q-toggle
-                                    v-model="
-                                        localCourse.user_config[
-                                            'notify_deadline_1_day'
-                                        ]
-                                    "
-                                    color="bk"
-                                    checked-icon="fa-solid fa-check"
-                                    unchecked-icon="fa-solid fa-times"
-                                />
-                            </div>
-
-                            <div
-                                class="full-width row justify-between items-center text-w"
-                            >
-                                <span>Notificar prazo de entraga (no dia)</span>
-                                <q-toggle
-                                    v-model="
-                                        localCourse.user_config[
-                                            'notify_deadline'
-                                        ]
-                                    "
-                                    color="bk"
-                                    checked-icon="fa-solid fa-check"
-                                    unchecked-icon="fa-solid fa-times"
-                                />
-                            </div>
-
-                            <div
-                                class="full-width row justify-between items-center text-w"
-                            >
-                                <span>Notificar envio de entraga</span>
-                                <q-toggle
-                                    v-model="
-                                        localCourse.user_config[
-                                            'notify_send_assignment'
-                                        ]
-                                    "
-                                    color="bk"
-                                    checked-icon="fa-solid fa-check"
-                                    unchecked-icon="fa-solid fa-times"
-                                />
-                            </div>
-                        </fieldset>
-                        <fieldset class="config-group">
-                            <legend>Mensagens e Comentarios</legend>
-
-                            <div
-                                class="full-width row justify-between items-center text-w"
-                            >
-                                <span>Notificar recebimento de mensagens</span>
-                                <q-toggle
-                                    v-model="
-                                        localCourse.user_config[
-                                            'notify_receive_message'
-                                        ]
-                                    "
-                                    color="bk"
-                                    checked-icon="fa-solid fa-check"
-                                    unchecked-icon="fa-solid fa-times"
-                                />
-                            </div>
-
-                            <div
-                                class="full-width row justify-between items-center text-w"
-                            >
-                                <span
-                                    >Notificar recebimento de comentarios</span
-                                >
-                                <q-toggle
-                                    v-model="
-                                        localCourse.user_config[
-                                            'notify_receive_comment'
-                                        ]
-                                    "
-                                    color="bk"
-                                    checked-icon="fa-solid fa-check"
-                                    unchecked-icon="fa-solid fa-times"
-                                />
-                            </div>
-
-                            <div
-                                class="full-width row justify-between items-center text-w"
-                            >
-                                <span>Notificar exclusão de comentarios</span>
-                                <q-toggle
-                                    v-model="
-                                        localCourse.user_config[
-                                            'notify_delete_comment'
-                                        ]
-                                    "
-                                    color="bk"
-                                    checked-icon="fa-solid fa-check"
-                                    unchecked-icon="fa-solid fa-times"
-                                />
-                            </div>
-                        </fieldset>
+                    <div
+                        class="full-width row justify-between items-center text-w"
+                    >
+                        <span>Notificar exclusão de conteudo</span>
+                        <q-toggle
+                            v-model="
+                                localCourse.user_config[
+                                    'notify_delete_content'
+                                ]
+                            "
+                            color="bk"
+                            checked-icon="fa-solid fa-check"
+                            unchecked-icon="fa-solid fa-times"
+                        />
                     </div>
-                </q-scroll-area>
-            </q-card-section>
-        </q-card>
-    </q-dialog>
+                </fieldset>
+                <fieldset class="config-group">
+                    <legend>Prazo</legend>
+
+                    <div
+                        class="full-width row justify-between items-center text-w"
+                    >
+                        <span>Notificar prazo de entraga (2 dias)</span>
+                        <q-toggle
+                            v-model="
+                                localCourse.user_config[
+                                    'notify_deadline_2_days'
+                                ]
+                            "
+                            color="bk"
+                            checked-icon="fa-solid fa-check"
+                            unchecked-icon="fa-solid fa-times"
+                        />
+                    </div>
+
+                    <div
+                        class="full-width row justify-between items-center text-w"
+                    >
+                        <span>Notificar prazo de entraga (1 dia)</span>
+                        <q-toggle
+                            v-model="
+                                localCourse.user_config[
+                                    'notify_deadline_1_day'
+                                ]
+                            "
+                            color="bk"
+                            checked-icon="fa-solid fa-check"
+                            unchecked-icon="fa-solid fa-times"
+                        />
+                    </div>
+
+                    <div
+                        class="full-width row justify-between items-center text-w"
+                    >
+                        <span>Notificar prazo de entraga (no dia)</span>
+                        <q-toggle
+                            v-model="
+                                localCourse.user_config[
+                                    'notify_deadline'
+                                ]
+                            "
+                            color="bk"
+                            checked-icon="fa-solid fa-check"
+                            unchecked-icon="fa-solid fa-times"
+                        />
+                    </div>
+
+                    <div
+                        class="full-width row justify-between items-center text-w"
+                    >
+                        <span>Notificar envio de entraga</span>
+                        <q-toggle
+                            v-model="
+                                localCourse.user_config[
+                                    'notify_send_assignment'
+                                ]
+                            "
+                            color="bk"
+                            checked-icon="fa-solid fa-check"
+                            unchecked-icon="fa-solid fa-times"
+                        />
+                    </div>
+                </fieldset>
+                <fieldset class="config-group">
+                    <legend>Mensagens e Comentarios</legend>
+
+                    <div
+                        class="full-width row justify-between items-center text-w"
+                    >
+                        <span>Notificar recebimento de mensagens</span>
+                        <q-toggle
+                            v-model="
+                                localCourse.user_config[
+                                    'notify_receive_message'
+                                ]
+                            "
+                            color="bk"
+                            checked-icon="fa-solid fa-check"
+                            unchecked-icon="fa-solid fa-times"
+                        />
+                    </div>
+
+                    <div
+                        class="full-width row justify-between items-center text-w"
+                    >
+                        <span
+                            >Notificar recebimento de comentarios</span
+                        >
+                        <q-toggle
+                            v-model="
+                                localCourse.user_config[
+                                    'notify_receive_comment'
+                                ]
+                            "
+                            color="bk"
+                            checked-icon="fa-solid fa-check"
+                            unchecked-icon="fa-solid fa-times"
+                        />
+                    </div>
+
+                    <div
+                        class="full-width row justify-between items-center text-w"
+                    >
+                        <span>Notificar exclusão de comentarios</span>
+                        <q-toggle
+                            v-model="
+                                localCourse.user_config[
+                                    'notify_delete_comment'
+                                ]
+                            "
+                            color="bk"
+                            checked-icon="fa-solid fa-check"
+                            unchecked-icon="fa-solid fa-times"
+                        />
+                    </div>
+                </fieldset>
+            </div>
+        </q-scroll-area>
+    </modal-template>
+
 </template>
 
 <script lang="ts">
 import { CourseDTO } from "src/@types/dtos";
 import { defineComponent, PropType } from "vue";
 
+import ModalTemplate from "./ModalTemplate.vue";
+
 export default defineComponent({
     name: "ModalEdit",
+    components: {
+        ModalTemplate
+    },
     props: {
         value: {
             type: Boolean,
@@ -262,12 +250,6 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.q-card {
-    max-width: none;
-    width: 95%;
-    border-radius: 20px;
-}
-
 .config-group {
     display: flex;
     flex-direction: column;
