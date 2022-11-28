8bc1f49ae90a567778f0e0905a63b58e17f3d612
Emanuel-IFSUL <emanuelscherer.ssnfq@academico.ifsul.edu.br>
2022-11-28 10:03:19 -0300

    init perfil

---
+++ src/main/kotlin/klaza/klaza_server/views/src/pages/PerfilPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <q-page class="row items-center justify-evenly">
+        <div>
+            <q-avatar size="xl">
+                <!-- //TODO - colocar a imagem do usuário -->
+                <img src="https://cdn.quasar.dev/img/avatar.png" />
+            </q-avatar>
+
+            <span> {{ user.username }} </span>
+            <span> {{ user.role }} </span>
+        </div>
+    </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useUserStore } from "stores/user";
+
+export default defineComponent({
+    name: "PerfilPage",
+    setup() {
+        return {
+            user: useUserStore().user,
+        };
+    },
+});
+</script>
+
+<style lang="scss" scoped></style>
